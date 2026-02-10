33e5e0a31520a00a4bcf5d54098884b6e7aea2e3
Lusine13 <lusinevardanyan.a@gmail.com>
2026-02-10 00:15:46 -0800

    chore: setup firebase configuration

---
+++ src/config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKSqTNc4fHcMhYWeEwMidRQN-EnbB-0zA",
+  authDomain: "jira-228e9.firebaseapp.com",
+  projectId: "jira-228e9",
+  storageBucket: "jira-228e9.appspot.com",
+  messagingSenderId: "871686075901",
+  appId: "1:871686075901:web:f8105f87068e4c1c145594"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
